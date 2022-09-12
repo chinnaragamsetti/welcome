23ecf7a3be067d3fc6dc77d0020c79c200c61908
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2022-09-12 11:35:28 +0530

    Update index.js

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,28 +1,4 @@
 // Write your code himport { Component } from "react"
-import {Component} from 'react'
-
-import './index.css'
-
-class Welcome extends Component {
-  state = {isSubscribed: true}
-  subscribedStatusBtn = () => {
-    const {isSubscribed} = this.state
-    if (isSubscribed === true) {
-      return <button className="butt">Subscribe</button>
-    }
-    return <button className="butt">Subscribed</button>
-  }
-  render() {
-    return (
-      <div className="bg">
-        <h1 className="h1">Welcome</h1>
-        <p className="p">Thank You! Happy Learning</p>
-        {this.subscribedStatusBtn()}
-      </div>
-    )
-  }
-}
-export default Welcome
 
 import {Component} from 'react'
 

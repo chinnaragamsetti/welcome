41f3db03353c9dd560b50d23053865e6509abf0d
chinnaragamsetti <110217256+chinnaragamsetti@users.noreply.github.com>
2022-09-12 11:32:43 +0530

    Update index.js

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -23,3 +23,46 @@ class Welcome extends Component {
   }
 }
 export default Welcome
+
+import {Component} from 'react'
+
+import './index.css'
+
+class Welcome extends Component {
+  state = {status:Subscribe}
+  subscribedStatusBtn=()=>{
+      const {status} = this.state
+    if (status===Subscribe) {
+      return(
+          this.setState((prevState)=>({prevState.status:Subscribed}))
+      )
+      
+    }
+     return
+     (
+          this.setState((prevState)=>({prevState.status:Subscribe}))
+     )
+      
+  
+
+      
+  }
+
+  render() {
+    const {status} =this.state
+    return (
+      <div className="bg">
+        <h1 className="h1">Welcome</h1>
+        <p className="p">Thank You! Happy Learning</p>
+        <button
+          onClick={this.subscribedStatusBtn}
+          type="button"
+          className="butt"
+        >
+        {status}
+        </button>
+      </div>
+    )
+  }
+}
+export default Welcome
